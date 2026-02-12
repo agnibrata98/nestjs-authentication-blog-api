b2e0086bdd7bf43db857712ccf3dee9c12425b64
agnibrata98 <agnibratashee@gmail.com>
2026-02-12 15:38:57 +0530

    adding vercel domain in cors policy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://nextjs-shadcn-website-design.vercel.app'],
     credentials: true,
   });
 
